64e1bb2a863c0eb21ba9ec0d946112052827930f
AnneSilva97 <silva.annecarol@gmail.com>
2024-11-12 16:15:49 -0300

    Adicionando sessão de video ao home (imagem ilustrativa).

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -4,87 +4,128 @@ import image from '../../assets/image.svg'
 import gears from '../../assets/gears.svg'
 import heartbeat from '../../assets/heartbeat.svg'
 import chef_hat from '../../assets/chef-hat.svg'
+import video_apresentacao from '../../assets/video-apresentacao.svg'
 
-import styles from './home.module.css' 
+import styles from './home.module.css'
 
 
-function Home () {
-    return(
+
+function Home() {
+    return (
         <div>
-      <div className={styles.title}>
-        <h2>Buscando algum serviço?</h2>
-      </div>
+            
+                <div className={styles.title}>
+                    <h2>Conheça um pouco a +Delas:</h2>
+                </div>
+                <div className={styles.video}>
+                    <img className={styles.videoApresentacao} src={video_apresentacao} alt="foto-video" />
 
-      <div className={styles.container}> 
+                </div>
 
-        <div className={styles.fileira}>
+                <div className={styles.title}>
+                    <h2>Buscando algum serviço?</h2>
+                </div>
 
-          <div className={styles.quadrado}>
-            <img className={styles.imgService} src={cosmetics} alt="cosmetics" />
-            <h3 className={styles.titleService} >Moda e Beleza</h3>
-            <p className={styles.descService}>Consultoria de Estilo,
-              Maquiagem Profissional,
-              Cabeleireira, Estilização
-              Serviços de Estética e muito mais</p>
-              <button className={styles.buttonService}>Ache uma Profissional</button>
-          </div>
+                <div className={styles.container}>
 
-          <div className={styles.quadrado}>
-            <img className={styles.imgService} src={front_end_programming} alt="tecnologia" />
-            <h3 className={styles.titleService} >Design e Tecnologia</h3>
-            <p className={styles.descService}>Soluções em design gráfico,
-              desenvolvimento de sites e inovação tecnológica,
-              oferecendo serviços que atende às suas necessidades.</p>
-              <button className={styles.buttonService}>Ache uma Profissional</button>
-          </div>
+                    <div className={styles.fileira}>
 
-          <div className={styles.quadrado}>
-            <img className={styles.imgService} src={image} alt="calendario" />
-            <h3 className={styles.titleService} >Eventos</h3>
-            <p className={styles.descService}>Planejamento de Eventos,
-              decoração e ambientação,
-              coordenação de festas,
-              fotografia geral. Tudo para deixar o momento mais incrível!</p>
-              <button className={styles.buttonService}>Ache uma Profissional</button>
-          </div>
+                        <div className={styles.quadrado}>
+                            <div className={styles.top}>
+                                <img className={styles.imgService} src={cosmetics} alt="cosmetics" />
+                                <h3 className={styles.titleService} >Moda e Beleza</h3>
+                                <p className={styles.descService}>Consultoria de Estilo,
+                                    Maquiagem Profissional,
+                                    Cabeleireira, Estilização
+                                    Serviços de Estética e muito mais</p>
+                            </div>
+                            <div className={styles.bottom}>
+                                <button className={styles.buttonService}>Ache uma Profissional</button>
+                            </div>
+                        </div>
+
+                        <div className={styles.quadrado}>
+                            <div className={styles.top}>
+                                <img className={styles.imgService} src={front_end_programming} alt="tecnologia" />
+                                <h3 className={styles.titleService} >Design e Tecnologia</h3>
+                                <p className={styles.descService}>Soluções em design gráfico,
+                                    desenvolvimento de sites e inovação tecnológica,
+                                    oferecendo serviços que atende às suas necessidades.</p>
+                            </div>
+                            <div className={styles.bottom}>
+                                <button className={styles.buttonService}>Ache uma Profissional</button>
+                            </div>
+                        </div>
+
+                        <div className={styles.quadrado}>
+                            <div className={styles.top}>
+                                <img className={styles.imgService} src={image} alt="calendario" />
+                                <h3 className={styles.titleService} >Eventos</h3>
+                                <p className={styles.descService}>Planejamento de Eventos,
+                                    decoração e ambientação,
+                                    coordenação de festas,
+                                    fotografia geral. Tudo para deixar o momento mais incrível!</p>
+                            </div>
+                            <div className={styles.bottom}>
+                                <button className={styles.buttonService}>Ache uma Profissional</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className={styles.fileira}>
+
+                        <div className={styles.quadrado}>
+                            <div className={styles.top}>
+                                <img className={styles.imgService} src={gears} alt="assistencia" />
+                                <h3 className={styles.titleService} >Assistência Técnica</h3>
+                                <p className={styles.descService}>Serviços de assistência técnica que você pode confiar.
+                                    Reparos precisos para garantir a longevidade dos seus equipamentos!</p>
+                            </div>
+
+                            <div className={styles.bottom}>
+                                <button className={styles.buttonService}>Ache uma Profissional</button>
+                            </div>
+                        </div>
+
+                        <div className={styles.quadrado}>
+                            <div className={styles.top}>
+                                <img className={styles.imgService} src={heartbeat} alt="coracao" />
+                                <h3 className={styles.titleService} >Saúde e Bem-estar</h3>
+                                <p className={styles.descService}>Terapias Holísticas,
+                                    personal trainer,
+                                    nutrição e dietética
+                                    aconselhamento psicológico, etc. </p>
+                            </div>
+                            <div className={styles.bottom}>
+                                <button className={styles.buttonService}>Ache uma Profissional</button>
+                            </div>
+                        </div>
+
+                        <div className={styles.quadrado}>
+                            <div className={styles.top}>
+                                <img className={styles.imgService} src={chef_hat} alt="chapeu" />
+                                <h3 className={styles.titleService} >Gastronomia</h3>
+                                <p className={styles.descService}>Encontre o melhor da gastronomia na sua região: Serviços de Buffet,
+                                    cozinheiras e chefs,
+                                    aulas de culinária e muito mais</p>
+                            </div>
+                            <div className={styles.bottom}>
+                                <button className={styles.buttonService}>Ache uma Profissional</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            
 
         </div>
-
-        <div className={styles.fileira}>
-
-          <div className={styles.quadrado}>
-            <img className={styles.imgService} src={gears} alt="assistencia" />
-            <h3 className={styles.titleService} >Assistência Técnica</h3>
-            <p className={styles.descService}>Serviços de assistência técnica que você pode confiar.
-              Reparos precisos para garantir a longevidade dos seus equipamentos!  </p>
-              <button className={styles.buttonService}>Ache uma Profissional</button>
-          </div>
-
-          <div className={styles.quadrado}>
-            <img className={styles.imgService} src={heartbeat} alt="coracao" />
-            <h3 className={styles.titleService} >Saúde e Bem-estar</h3>
-            <p className={styles.descService}>Terapias Holísticas,
-              personal trainer,
-              nutrição e dietética
-              aconselhamento psicológico, etc. </p>
-              <button className={styles.buttonService}>Ache uma Profissional</button>
-          </div>
-
-          <div className={styles.quadrado}>
-            <img className={styles.imgService} src={chef_hat} alt="chapeu" />
-            <h3 className={styles.titleService} >Gastronomia</h3>
-            <p className={styles.descService}>Encontre o melhor da gastronomia na sua região: Serviços de Buffet,
-              cozinheiras e chefs,
-              aulas de culinária e muito mais</p>
-              <button className={styles.buttonService}>Ache uma Profissional</button>
-          </div>
-
-        </div>
-        
-      </div>
-
-    </div>
     )
 }
 
 export default Home;
+
+
+
+
