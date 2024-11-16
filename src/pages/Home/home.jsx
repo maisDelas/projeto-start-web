9d32d2e17db530699435484e4d4d66a9db3f7edf
AnneSilva97 <silva.annecarol@gmail.com>
2024-11-15 21:10:24 -0300

    instalando o pacote Styled Components

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -16,9 +16,9 @@ function Home() {
             {/* Sessão de Titulo e Video */}
 
             <div className={styles.title}>
-                <h2>Conheça um pouco a +Delas:</h2> 
+                <h2>Conheça um pouco a +Delas:</h2>
             </div>
-            <div className={styles.bgcards}> 
+            <div className={styles.bgcards}>
 
                 <div className={styles.videoContainer}>
                     <iframe className={styles.video} width="600" height="360" src="https://www.youtube.com/embed/UXwPz61lxBY?si=kMwtBZjDrElhp0lP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
