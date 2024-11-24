dfe293d6769f02b473ea1998d8b30fe680e7899d
Djair <djair1936@outlook.com>
2024-11-24 15:37:10 -0300

    perfil e cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Trilha from 'pages/trilha/Trilha'
+import Perfil from 'pages/perfil/Perfil'
+import Cadastro from 'cadastro/Cadastro'
+
 
 
 export default function App() {
@@ -9,8 +12,11 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Trilha" element={< Trilha/>} />
-        
+        <Route path="/Perfil" element={< Perfil />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
       </Routes>
       </BrowserRouter>
   )
 }
+
+
