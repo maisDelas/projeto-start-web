785aaa5d830b50da79e1e49abb89270afd464b24
Djair <djair1936@outlook.com>
2024-11-27 22:38:00 -0300

    atualização do footet mais paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from 'pages/home/Home'
+import Home from 'pages/home/home'
 import Mapeamento from 'pages/mapeamento/Mapa'
 import Cards from 'pages/mapeamento/Cards'
 import Trilha from 'pages/trilha/Trilha'
 import Perfil from 'pages/perfil/Perfil'
 import ConteudoTrilha1 from './pages/trilha/TrilhaConteudo1'
 import ConteudoTrilha2 from 'pages/trilha/TrilhaConteudo2'
-
 import Dados from 'pages/dados/Dados'
-
 import CadastroTipo from 'pages/escolhercadastro/CadastroTipo'
 import Login from 'pages/login/Login'
 import ResetPassword from 'pages/resetPassword/ResetPassword'
@@ -16,12 +14,8 @@ import CadastroPrestadora1 from 'pages/cadastroP/RegistrationFrm'
 import CadastroPrestadora2 from 'pages/cadastroP/Prestadoratre2'
 import CadastroPrestadora3 from 'pages/cadastroP/Prestadora3'
 import CadastroPrestadora0 from 'pages/cadastroP/Prestadora0'
-<<<<<<< HEAD
 import Pessoais from "./pages/pessoais/Pessoais";
-
-=======
 import Configuracao from 'pages/configuracao/Config'
->>>>>>> 7557d1fa0dc07de7ab0fa42b3051a3d8391fc0b7
 
   
 
@@ -39,20 +33,17 @@ export default function App() {
         <Route path="/Dados" element={<Dados/>} />
        
         <Route path="/Perfil" element={< Perfil />} />
-<<<<<<< HEAD
         <Route path="/Dados" element={<Dados />} />
         <Route path="/CadastroPrestadora0" element={<CadastroPrestadora0 />} />
         <Route path="CadastroPrestadora1" element={<CadastroPrestadora1 />} />
         <Route path="/CadastroPrestadora2" element={<CadastroPrestadora2 />} />
         <Route path="/CadastroPrestadora3" element={<CadastroPrestadora3 />} />
-=======
         <Route path="/Configuracao" element={<Configuracao/>}/>
         <Route path="/Dados" element={<Dados />} />;
         <Route path="/CadastroPrestadora0" element={<CadastroPrestadora0 />} />; 
         <Route path="CadastroPrestadora1" element={<CadastroPrestadora1 />} />;
         <Route path="/CadastroPrestadora2" element={<CadastroPrestadora2 />} />;
         <Route path="/CadastroPrestadora3" element={<CadastroPrestadora3 />} />; 
->>>>>>> 7557d1fa0dc07de7ab0fa42b3051a3d8391fc0b7
         <Route path="/Login" element={<Login />} />;
         <Route path="/ResetPassword" element={<ResetPassword />} />;
         <Route path="/CadastroTipo" element={<CadastroTipo />} />;
